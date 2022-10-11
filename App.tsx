6a2efa870ac24cb891f8218aa2f7b7c94789bed9
Katz <williankatz@gmail.com>
2022-10-11 17:18:38 -0300

    Novas telas e componentização

--- App.tsx
+++ App.tsx
@@ -1,7 +1,24 @@
+import { StatusBar } from 'react-native'
+import { ThemeProvider } from 'styled-components';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { Groups } from '@screens/Groups';
+import theme from './src/theme'
+import { Loading } from '@components/Loading';
+import { NewGroup } from '@screens/NewGroup';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+
+
   return (
-    <Groups />
+    <ThemeProvider theme={theme}>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      { fontsLoaded ? <NewGroup /> : <Loading />}
+    </ThemeProvider>
   );
 }
